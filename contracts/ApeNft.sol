493c2cedd4e3eaeecb98fce6053bfde0595d1ba0
Ivan <block4ain226@gmail.com>
2023-08-29 14:27:07 +0300

    init

--- contracts/ApeNft.sol
+++ contracts/ApeNft.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Lock {
+contract ApeNft {
     uint public unlockTime;
     address payable public owner;
 
