776b2fd1e1776e19eb4ebec30787b53a3de04758
Ivan <block4ain226@gmail.com>
2023-08-30 20:23:30 +0300

    add new functions

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 const {initializeApp} = require("firebase/app");
-const {getFirestore, collection, getDocs, doc, setDoc} = require("firebase/firestore");
+const {getFirestore, collection, getDocs, doc,arrayUnion,updateDoc, setDoc,arrayRemove} = require("firebase/firestore");
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyCpOEM5_RxgIyfhSncty9O7oHLpvFUbP20",
@@ -13,7 +13,7 @@ const firebaseApp = initializeApp({
 const db = getFirestore(firebaseApp);
 
 async function getCities(db) {
-    const citiesCol = collection(db, 'usersContractsIds');
+    const citiesCol = collection(db, 'usersNfts/0xBDf761788135C7d7Aa76E6671f63462A07C53E2C/0xBDf761788135C7d7Aa76E6671f63462A07C53E2D');
     const citySnapshot = await getDocs(citiesCol);
     const cityList = citySnapshot.docs.map(doc => doc.data());
     return cityList;
@@ -21,11 +21,24 @@ async function getCities(db) {
 
 async function write(db) {
     const data = ({contract: "0xBDf761788135C7d7Aa76E6671f63462A07C53E2D", value: [2, 6]});
-    const res = await setDoc(doc(db, "usersContractsIds", "12"), data);
+    const res = await setDoc(doc(db, "usersNfts", "0xBDf761788135C7d7Aa76E6671f63462A07C53E2R"), data);
+}
+
+async function update(db) {
+    const data = ({contract: "0xBDf761788135C7d7Aa76E6671f63462A07C53E2N", value: [2,  7]});
+    const res = await updateDoc(doc(db, "usersNfts", "0xBDf761788135C7d7Aa76E6671f63462A07C53E2R"), data);
+}
+async function updateArray(db) {
+    const data = ({value: 10});
+    const res = await updateDoc(doc(db, "usersNfts", "0xBDf761788135C7d7Aa76E6671f63462A07C53E2R"), {value: arrayUnion(10)});
+}
+async function removeFromArray(db) {
+    const data = ({value: 10});
+    const res = await updateDoc(doc(db, "usersNfts", "0xBDf761788135C7d7Aa76E6671f63462A07C53E2R"), {value: arrayRemove(2)});
 }
 
 getCities(db).then(data => console.log(data));
-write(db);
+removeFromArray(db);
 
 
 // Initialize Firebase
